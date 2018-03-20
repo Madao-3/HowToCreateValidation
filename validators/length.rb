ef693c74fe254093e38dd2ea14864fd053c5a9e0
Madao-3 <chrishyman256@gmail.com>
2018-03-20 17:41:58 +0800

    add new validator

--- validators/length.rb
+++ validators/length.rb
@@ -1,16 +1,6 @@
-class LengthValidator < Validator
-  CHECKS = {:is=>:==, :minimum=>:>=, :maximum=>:<=}
-
+class AbsenceValidator < Validator
   def valid?(obj)
-    value = obj.send(@attr_name)
-    value_length = value.respond_to?(:length) ? value.length : value.to_s.length
-    CHECKS.each do |key, check_key|
-      @option.each do |option_key, option_value|
-        next unless CHECKS[option_key]
-        p "#{value_length},#{CHECKS[option_key]},#{option_value}"
-        return false unless value_length.send(CHECKS[option_key], option_value)
-      end
-    end
-    true
+    result = obj.send(@attr_name).present?
+    @option ? result : !result
   end
 end
